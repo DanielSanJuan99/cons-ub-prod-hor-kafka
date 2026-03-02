c0c8286f463ae37e5bd100586ad76dd7ee04461d
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-03-01 22:59:01 -0300

    feat: agregar estructura base del proyecto Spring Boot

---
+++ src/main/java/com/duoc/cons_ub_prod_hor_kafka/ConsUbProdHorKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.duoc.cons_ub_prod_hor_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsUbProdHorKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsUbProdHorKafkaApplication.class, args);
+	}
+}
